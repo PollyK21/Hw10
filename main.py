4803bdfe2232b900e450369d995671774f07948c
PollyK21 <pol.kulish@gmail.com>
2023-12-16 17:01:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not (len(value) == 10 and value.isdigit()):
+            raise ValueError
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        new_phone = Phone(phone)
+        self.phones.append(new_phone)
+
+    def remove_phone(self, phone):
+        for ph in self.phones:
+            if ph.value == phone:
+                self.phones.remove(ph)
+
+    def edit_phone(self, old_phone, new_phone):
+        if old_phone in [p.value for p in self.phones]:
+            self.remove_phone(old_phone)
+            self.add_phone(new_phone)
+        else:
+            raise ValueError
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            self.data.pop(name)
